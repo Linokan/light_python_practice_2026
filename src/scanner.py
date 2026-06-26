86ec64305bd50e83a996a439aa1b32c87344f745
Linokan <andrei125.ivanov@Mail.ru>
2026-06-26 12:07:32 +0700

    заменил rglob на обычную рекурсивную функцию

--- src/scanner.py
+++ src/scanner.py
@@ -1,30 +1,49 @@
 from pathlib import Path
+
 from duplicates import calculate_file_hash
 
 
 def scan_folder(folder, extension_filter=None):
     files_data = []
 
-    for file_path in folder.rglob("*"):
-        if ".git" in file_path.parts:
-            continue
-
-        if "__pycache__" in file_path.parts:
-            continue
-
-        if file_path.is_file():
-
-            if extension_filter and file_path.suffix != extension_filter:
-                continue
-
-            stat = file_path.stat()
-
-            files_data.append({
-                "relative_path": str(file_path.relative_to(folder)),
-                "size": stat.st_size,
-                "modified_time": stat.st_mtime,
-                "extension": file_path.suffix,
-                "file_hash": calculate_file_hash(file_path)
-            })
+    scan_recursive(
+        folder,
+        folder,
+        extension_filter,
+        files_data
+    )
 
     return files_data
+
+
+def scan_recursive(current_folder, root_folder, extension_filter, files_data):
+    for item in current_folder.iterdir():
+
+        if item.name == ".git":
+            continue
+
+        if item.name == "__pycache__":
+            continue
+
+        if item.is_dir():
+            scan_recursive(
+                item,
+                root_folder,
+                extension_filter,
+                files_data
+            )
+
+        elif item.is_file():
+
+            if extension_filter and item.suffix != extension_filter:
+                continue
+
+            stat = item.stat()
+
+            files_data.append({
+                "relative_path": str(item.relative_to(root_folder)),
+                "size": stat.st_size,
+                "modified_time": stat.st_mtime,
+                "extension": item.suffix,
+                "file_hash": calculate_file_hash(item)
+            })
